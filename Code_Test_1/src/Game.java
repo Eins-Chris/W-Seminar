383901e583d030a46d0ee8a3d451db2f9bcf320c
Chris <scharl.chrissi2006@gmail.com>
2025-01-06 03:38:55 +0100

    working V0.1

--- Code_Test_1/src/Game.java
+++ Code_Test_1/src/Game.java
@@ -1,9 +1,13 @@
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Game extends Kaestchen {
 
     private float[][] matrix = new float[100][100];
-    private final int START = 20;
+    private final int STARTX = 20;
+    private final int STARTY = 20;
 
     public static void main(String[] args) {
         new Game().start();
@@ -15,51 +19,101 @@ public class Game extends Kaestchen {
 
     public void start() {
         initialize();
-        //move(START,START);
+        move(STARTX,STARTY,STARTX,STARTY);
         print();
     }
 
+    public void initialize() {
+        // start placement
+        matrix[STARTX][STARTY] = 9.9f;
+    }
+
+    public void move(int x, int y, int ox, int oy) {
+        // getting neighbours
+        int[] startup = {-ox+x, -oy+y};
+        //  System.out.println("X: "+x+" | Y: "+y);
+        //  System.out.println("StartupX: "+-ox+"+"+x +" | StartupY: "+-oy+"+"+y);
+        List<int[]> neighbours = getNeighbours(x, y, (int) Math.ceil(getPower(x, y) / 5.0), startup);
+
+        // setting neighbour to -1 power and 9 time & move
+        for (int[] neighbour : neighbours) {
+            //  System.out.println("neighbour" + Arrays.toString(neighbour));
+            //  System.out.println("Calling: x=" + neighbour[0] + ", y=" + neighbour[1] + ", ox=" + x + ", oy=" + y);
+            matrix[x + neighbour[0]][y + neighbour[1]] = getPower(x, y) - 1 + 0.9f;
+            move(x + neighbour[0], y + neighbour[1], x, y);
+        }
+    }
+
+    public List<int[]> getNeighbours(int x, int y, int quantity, int[] startup) {
+        int sx = startup[0]+1;
+        int sy = startup[1]+1;
+
+        int[][][] localmatrix = {
+            {{-1,-1}, {-1, 0}, {-1, 1}},
+            {{ 0,-1}, { 0, 0}, { 0, 1}},
+            {{ 1,-1}, { 1, 0}, { 1, 1}},
+        };
+        int[][] directions = {
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1}, // direkte Nachbarn oben, unten, links, rechts
+            {-1, -1}, {-1, 1}, {1, -1}, {1, 1} // diagonale Nachbarn
+        };
+
+        List<int[]> neighbours = new ArrayList<>();
+        for (int[] dir : directions) {
+            int nx = sx + dir[0];
+            int ny = sy + dir[1];
+            if (nx >= 0 && nx < localmatrix.length && ny >= 0 && ny < localmatrix[0].length) {
+                neighbours.add(new int[]{localmatrix[nx][ny][0], localmatrix[nx][ny][1]});
+            }
+        }
+        //  System.out.println("Sx: " + sx + " | Sy: " + sy);
+        neighbours.add(new int[]{localmatrix[sx][sy][0], localmatrix[sx][sy][1]});
+
+        neighbours.removeIf(neighbour -> neighbour[0] == 0 && neighbour[1] == 0);
+
+        Collections.shuffle(neighbours);
+        
+        return neighbours.subList(0, Math.min(quantity, neighbours.size()));
+    }
+
     public void print() {
-        for (int i = 1; i < 40; i++) {
-            for (int j = 1; j < 40; j++) {
-                farbeSetzen(i, j, getColor(matrix[i-1][j-1]));
-                System.out.print(matrix[i-1][j-1] + "|");
+        for (int y = 1; y <= 40; y++) {
+            for (int x = 1; x <= 40; x++) {
+                farbeSetzen(x, y, getColor(x, y));
+                //System.out.print(matrix[x-1][y-1] + "|");
+                System.out.print(getPower(x, y) + "|");
             }
             System.out.println();
         }
     }
 
-    public Color getColor(float input) {
-        switch ((int) (input*10)) {
-            case 10:
+    public Color getColor(int x, int y) {
+        switch (getPower(x, y)) {
+            case 9:
                 return Color.GREEN;
-            default:
+            case 8:
+            case 7:
+            case 6:
+                return Color.YELLOW;
+            case 5:
+            case 4:
+            case 3:
+            case 2:
+            case 1:
+                return Color.ORANGE;
+            case 0:
                 return Color.WHITE;
+            default:
+                return Color.LIGHT_GRAY;
         }
     }
 
-    public void initialize() {
-        //Blob placement
-        matrix[START][START] = 1;
+    public int getPower(int x, int y) {
+        return (int) Math.floor(matrix[x][y]);
     }
 
-    public float[][] getNeighbours(int x, int y) {
-        float[][] result = new float[3][3];
-        result[0][0] = matrix[x-1][y-1];
-        result[1][0] = matrix[x][y-1];
-        result[2][0] = matrix[x+1][y-1];
-        result[0][1] = matrix[x-1][y];
-        result[1][1] = matrix[x][y];
-        result[2][1] = matrix[x+1][y];
-        result[0][2] = matrix[x-1][y+1];
-        result[1][2] = matrix[x][y+1];
-        result[2][2] = matrix[x+1][y+1];
-        return result;
-    }
-
-    public void move(int x, int y) {
-        // matrix[x][y] => für 1 in 3 oder 4 Richtungen, für weniger in 2 Richtungen...
-        // move ausführen für nachbaren in den genannten richtungen
+    public int getTime(int x, int y) {
+        return (int) ((matrix[x][y] - ((int) Math.floor(matrix[x][y]))) * 10);
     }
 
 }
